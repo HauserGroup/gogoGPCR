293655a5472ef23cac68629827e8c7414d96d12a
jsture <jakobsturemadsen@gmail.com>
2021-10-28 10:51:38 +0000

    fixed process results

---
+++ src/results.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def pval_stars(x: float):
+    if x <= 0.0001:
+        return "****"
+    elif x <= 0.001:
+        return "***"
+    elif x <= 0.01:
+        return "**"
+    elif x <= 0.05:
+        return "*"
+    if x > 0.05:
+        return ""
+    
+def pheno_search(x, ukb_coding: pd.DataFrame, custom_coding: pd.DataFrame):
+    ukb = dict(zip(ukb_coding.FieldID.astype(str), ukb_coding.Field))
+    custom = dict(zip(custom_coding.FieldID.astype(str), custom_coding.Field))
+    both = {**ukb, **custom}
+    
+    return both[str(x)]
+    
+    
+    
+    
