469c512aee51153fc054bff3faac65a76da68e55
jsture <jakobsturemadsen@gmail.com>
2021-11-29 20:54:33 +0000

    added phesant docker and step2

---
+++ docker/phesant/install_packages.R
@@ -0,0 +1,11 @@
+install.packages("devtools")
+
+library(devtools)
+
+install_version("optparse", version = "1.3.2")
+install_version("MASS", version = "7.3-45")
+install_version("data.table")
+install_version("lmtest", version = "0.9-34")
+install_version("nnet", version = "7.3-12")
+install_version("forestplot")
+
---
+++ scripts/2_regenie_step2_BT.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+gene="DRD2"
+phenotype="psychiatric"
+data_file_dir="/mnt/project/Data"
+
+run_regenie_step2="
+sed -i 's|/home/dnanexus/out/out/|${data_file_dir}/step1/${phenotype}.BT/|' ${phenotype}_step1_BT_pred.list &&
+sed -i 's|/home/dnanexus/out/out/|${data_file_dir}/step1/${phenotype}.BT/|' ${phenotype}_step1_BT_firth.list && 
+regenie \
+  --step 2 \
+  --bt \
+  --bgen ${data_file_dir}/burden/${gene}.bgen \
+  --sample ${data_file_dir}/burden/${gene}.sample \
+  --ref-first \
+  --phenoFile ${data_file_dir}/phenotypes/${phenotype}.tsv \
+  --covarFile ${data_file_dir}/phenotypes/covariates.tsv \
+  --firth --approx \
+  --use-null-firth ${phenotype}_step1_BT_firth.list \
+  --pred ${phenotype}_step1_BT_pred.list \
+  --bsize 200 \
+  --anno-file ${data_file_dir}/burden/${gene}.annotations \
+  --set-list ${data_file_dir}/burden/${gene}.setlist \
+  --mask-def ${data_file_dir}/burden/${gene}.masks \
+  --check-burden-files \
+  --out step2_BT_${phenotype}_${gene} \
+  --maxstep-null 1 \
+  --maxiter-null 25000 \
+  --write-samples \
+  --print-pheno \
+  --no-split \
+  --verbose
+"
+
+dx run swiss-army-knife -iin="/Data/step1/${phenotype}.BT/psychiatric_step1_BT_pred.list" \
+   -iin="/Data/step1/${phenotype}.BT/psychiatric_step1_BT_firth.list" \
+   -icmd="${run_regenie_step2}" --tag="Step1" --instance-type "mem1_ssd1_v2_x16"\
+   --destination="/Data/results" --yes;
